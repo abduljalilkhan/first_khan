a66436cb70f55ef72de4af319d3920172d90977a
Abudl Jalil Khan <abduljalilkhan1@gmail.com>
2023-03-28 14:17:58 +0500

    comment activity main
    comment activity  desc

--- app/src/main/java/com/example/githubmvvm/Exampleclass.java
+++ app/src/main/java/com/example/githubmvvm/Exampleclass.java
@@ -1,4 +1,15 @@
 package com.example.githubmvvm;
 
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.recyclerview.widget.RecyclerView;
+
 public class Exampleclass {
+    public RecyclerView recyclerView;
+
+    private ImageView imgDrawer;
+
+    private LinearLayout layout_Drawer, layoutXp;
+
 }
